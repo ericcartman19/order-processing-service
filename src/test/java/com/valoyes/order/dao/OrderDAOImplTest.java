455fb7a76e5260bdf730b01264e3751250688041
bavaloyes <bavaloyes@ard01.mioct.ext.sopra>
2018-05-03 17:14:56 +0200

    delete spring boot default class and TU

---
+++ src/test/java/com/valoyes/order/dao/OrderDAOImplTest.java
@@ -0,0 +1,14 @@
+package com.valoyes.order.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OrderDAOImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
