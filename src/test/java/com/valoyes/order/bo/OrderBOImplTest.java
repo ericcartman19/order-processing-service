8801591b8aa23d73f8b0fc7634156b4a6d643c34
bavaloyes <bavaloyes@ard01.mioct.ext.sopra>
2018-05-03 17:32:59 +0200

    add assert and verify

--- src/test/java/com/valoyes/order/bo/OrderBOImplTest.java
+++ src/test/java/com/valoyes/order/bo/OrderBOImplTest.java
@@ -1,5 +1,7 @@
 package com.valoyes.order.bo;
 
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.sql.SQLException;
@@ -9,6 +11,7 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import com.valoyes.order.bo.exception.BOException;
 import com.valoyes.order.dao.OrderDAO;
 import com.valoyes.order.dao.dto.Order;
 
@@ -24,13 +27,20 @@ public class OrderBOImplTest {
 	}
 	
 	@Test
-	public void placeOrder_Should_Create_An_Order() throws SQLException {
+	public void placeOrder_Should_Create_An_Order() throws SQLException, BOException {
+		// given
 		OrderBOImpl bo = new OrderBOImpl();
 		bo.setDao(dao);
 		
 		// when
+		// si no estamos testeando por exceptiones, simplemente las lanzamos
 		Order order = new Order();
 		when(dao.create(order)).thenReturn(1);
+		boolean resultado = bo.placeOrder(order);
+		
+		// then : comprobamos valos y verificamos la llamada del metodo
+		assertTrue(resultado);
+		verify(dao).create(order);
 	}
 
 }
