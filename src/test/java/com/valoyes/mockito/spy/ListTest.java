2a352c1ae8c7230826c2df37092bf08a77bc4a15
bavaloyes <bavaloyes@ITEM-S35533.emea.msad.sopra>
2018-05-06 10:09:57 +0200

    update

--- src/test/java/com/valoyes/mockito/spy/ListTest.java
+++ src/test/java/com/valoyes/mockito/spy/ListTest.java
@@ -1,13 +1,16 @@
 package com.valoyes.mockito.spy;
 
 import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 
@@ -15,6 +18,9 @@ public class ListTest {
 
 	@Spy
 	List<String> myList = new ArrayList<>();
+	
+	@Mock
+	List<String> myList2 = new ArrayList<>();
 	
 	@Before
 	public void init() {
@@ -30,6 +36,14 @@ public class ListTest {
 		doReturn(3).when(myList).size();
 		
 		assertSame(3, myList.size());
+	}
+	
+	@Test
+	public void test_MockWith_CallRealMethod() {
+		
+		when(myList2.get(0)).thenReturn("Rambo");
+		// when(myList2.size()).thenCallRealMethod();
+		assertSame(0, myList2.size());
 	}
 
 }
